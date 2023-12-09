65c581e1943edd4adb5662a2041c6faff7f2d1f3
An-dini <andini4533@gmail.com>
2023-12-09 23:55:55 +0700

    add toolbar

--- app/src/main/java/com/andini/neuroplay/view/AboutActivity.kt
+++ app/src/main/java/com/andini/neuroplay/view/AboutActivity.kt
@@ -2,12 +2,30 @@ package com.andini.neuroplay.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.andini.neuroplay.R
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
-        supportActionBar?.show()
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setTitleTextColor(resources.getColor(R.color.blue))
+        toolbar.navigationIcon?.setTint(ContextCompat.getColor(this@AboutActivity, R.color.blue))
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/andini/neuroplay/view/HistoryActivity.kt
+++ app/src/main/java/com/andini/neuroplay/view/HistoryActivity.kt
@@ -2,6 +2,9 @@ package com.andini.neuroplay.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.airbnb.lottie.LottieAnimationView
 import com.andini.neuroplay.R
 
@@ -10,12 +13,28 @@ class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-        supportActionBar?.show()
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setTitleTextColor(resources.getColor(R.color.blue))
+        toolbar.navigationIcon?.setTint(ContextCompat.getColor(this@HistoryActivity, R.color.blue))
 
         lottieAnimationView = findViewById(R.id.lottieAnimationView)
         lottieAnimationView.setAnimation("data_not_found.json")
         lottieAnimationView.playAnimation()
         lottieAnimationView.loop(true)
         lottieAnimationView.speed = 1.0f
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
